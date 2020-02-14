1b7c8a9a41f3d25c1b8a6a5bb0cdd1cedcad0492
João Paulo Guimarães <joao.paulo.f.guimaraes@gmail.com>
2020-02-14 15:40:51 -0200

    mudando exemplo

--- exemplo1.sh
+++ exemplo1.sh
@@ -2,11 +2,15 @@
 # Script para desmontar o arquivo executável
 # joao.guimaraes@ifrn.edu.br
 
+echo 'código super simples:'
+
 gcc -c super_simples.c
 
+##################################
 objdump -d super_simples.o
 
 echo 'Agora o código com uma única instrução'
+
 
 gcc -c simples.c 
 
